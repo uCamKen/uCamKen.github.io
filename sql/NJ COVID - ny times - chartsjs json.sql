19a3885e64994273f21d467a5a8faf24875b9075
Ken Klose <kenklose@gmail.com>
2020-05-25 11:16:49 -0400

    Moved some JS out of query

--- sql/NJ COVID - ny times - chartsjs json.sql
+++ sql/NJ COVID - ny times - chartsjs json.sql
@@ -2,7 +2,7 @@ select top 5 * from nytdata
 
 
 DECLARE @NewLineChar AS CHAR(2) = CHAR(13) + CHAR(10)
-select 'labels: [' + STUFF ((
+select 'var sqldata = {labels: [' + STUFF ((
 	select ',''' + date + ''''
 	from (
 		select date 
@@ -12,14 +12,7 @@ select 'labels: [' + STUFF ((
 	) as x
 	order by date
 	for xml path('')), 1, 1, '') + '],' + @NewLineChar + 
-	'datasets: [{' + @NewLineChar +
-	'    type: ''line'',' + @NewLineChar +
-	'    label: ''Total Cases'',' + @NewLineChar +
-	'    backgroundColor: ''rgb(255,99,132)'',' + @NewLineChar +
-	'    borderColor: ''rgb(255,99,132)'',' + @NewLineChar +
-	'    fill: false,' + @NewLineChar +
-	'    yAxisID: ''y-axis-1'',' + @NewLineChar + 
-	'    data: [' + STUFF ((
+	'    totalcases: [' + STUFF ((
 	select ',' + ISNULL(convert(varchar, cases),'')
 	from (
 		select state,date,sum("Total Cases") as cases
@@ -28,15 +21,8 @@ select 'labels: [' + STUFF ((
 		group by date, state
 	) as x
 	order by state, date
-	for xml path('')), 1, 1, '') + ']' + @NewLineChar +
-	'},{' + @NewLineChar +
-	'    type: ''line'',' + @NewLineChar +
-	'    label: ''New Cases Past 14 Days'',' + @NewLineChar +
-	'    backgroundColor: ''rgb(132,99,255)'',' + @NewLineChar +
-	'    borderColor: ''rgb(132,99,255)'',' + @NewLineChar +
-	'    fill: true,' + @NewLineChar +
-	'    yAxisID: ''y-axis-2'',' + @NewLineChar + 
-	'    data: [' + STUFF ((
+	for xml path('')), 1, 1, '') + '],' + @NewLineChar +
+	'    cases14days: [' + STUFF ((
 	select ',' + ISNULL(convert(varchar, cases),'')
 	from (
 		select state,date,sum("New Cases 14 Days") as cases
@@ -45,15 +31,8 @@ select 'labels: [' + STUFF ((
 		group by date, state
 	) as y
 	order by state, date
-	for xml path('')), 1, 1, '') + ']' + @NewLineChar +
-	'},{' + @NewLineChar +
-	'    type: ''bar'',' + @NewLineChar +
-	'    label: ''New Cases Today'',' + @NewLineChar +
-	'    backgroundColor: ''rgb(132,255,99)'',' + @NewLineChar +
-	'    borderColor: ''rgb(132,255,99)'',' + @NewLineChar +
-	'    fill: true,' + @NewLineChar +
-	'    yAxisID: ''y-axis-2'',' + @NewLineChar + 
-	'    data: [' + STUFF ((
+	for xml path('')), 1, 1, '') + '],' + @NewLineChar +
+	'    casesToday: [' + STUFF ((
 	select ',' + ISNULL(convert(varchar, cases),'')
 	from (
 		select state,date,sum("New Cases") as cases
@@ -63,6 +42,6 @@ select 'labels: [' + STUFF ((
 	) as y
 	order by state, date
 	for xml path('')), 1, 1, '') + ']' + @NewLineChar +
-	'}]' + @NewLineChar
+	'};' + @NewLineChar
 	as list
 
